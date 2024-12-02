cb8e0533502dc049001f9687060c70e7aeccedb3
akp-boom <iphoneatclass12@gmail.com>
2024-12-03 02:04:44 +0530

    Delete java.js

--- java.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('speakBtn').addEventListener('click', function() {
-    const msg = new SpeechSynthesisUtterance('CHUTIYA HA KYA');
-    window.speechSynthesis.speak(msg);
-});
